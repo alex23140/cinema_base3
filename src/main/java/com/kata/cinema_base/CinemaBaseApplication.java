1e4b2c9bdf104d4d28952e23050175d39736cd55
gusanimax <zaborskiy_ai@mail.ru>
2022-01-26 14:28:37 +0300

    инициализация проекта

---
+++ src/main/java/com/kata/cinema_base/CinemaBaseApplication.java
@@ -0,0 +1,13 @@
+package com.kata.cinema_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBaseApplication.class, args);
+    }
+
+}
