cad6dc5274b5f7b1bdfada4aea58306c236350ea
Gorshkov Sergey <Gsn82@mail.ru>
2022-02-05 21:22:38 +0500

    FEATURE - Функционал пользователя

--- src/main/java/com/kata/cinema/base/dao/impl/dto/PaginationDtoDaoImpl.java
+++ src/main/java/com/kata/cinema/base/dao/impl/dto/PaginationDtoDaoImpl.java
@@ -7,14 +7,15 @@ import java.util.List;
 import java.util.Map;
 
 @Repository
-public class PaginationDtoDaoImpl implements PaginationDtoDao {
+public class PaginationDtoDaoImpl<T> implements PaginationDtoDao<T> {
+
     @Override
-    public List getItemsDto(Integer currentPage, Integer itemsOnPage, Map parameters) {
+    public List<T> getItemsDto(Integer currentPage, Integer itemsOnPage, Map<String, Object> parameters) {
         return null;
     }
 
     @Override
-    public Long getResultTotal(Map parameters) {
+    public Long getResultTotal(Map<String, Object> parameters) {
         return null;
     }
 }
