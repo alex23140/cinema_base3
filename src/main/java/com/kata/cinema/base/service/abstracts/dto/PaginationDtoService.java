6042453ab9ec6adc59df3db90ae3fe2fe48b93ea
Gorshkov Sergey <Gsn82@mail.ru>
2022-02-07 08:16:15 +0500

    FEATURE - Функционал пользователя

--- src/main/java/com/kata/cinema/base/service/abstracts/dto/PaginationDtoService.java
+++ src/main/java/com/kata/cinema/base/service/abstracts/dto/PaginationDtoService.java
@@ -1,4 +1,4 @@
-package com.kata.cinema.base.service.abstracts;
+package com.kata.cinema.base.service.abstracts.dto;
 
 import com.kata.cinema.base.models.dto.PageDto;
 import org.springframework.data.domain.Page;
--- src/main/java/com/kata/cinema/base/service/impl/dto/PaginationDtoServiceImpl.java
+++ src/main/java/com/kata/cinema/base/service/impl/dto/PaginationDtoServiceImpl.java
@@ -2,7 +2,7 @@ package com.kata.cinema.base.service.impl.dto;
 
 import com.kata.cinema.base.dao.abstracts.dto.PaginationDtoDao;
 import com.kata.cinema.base.models.dto.PageDto;
-import com.kata.cinema.base.service.abstracts.PaginationDtoService;
+import com.kata.cinema.base.service.abstracts.dto.PaginationDtoService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
