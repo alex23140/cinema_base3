beca789e424c98f6c5ee518999cff0a656e7b2c5
Artem Shinkarenko <tomikus1@gmail.com>
2022-02-07 19:29:43 +0300

    тест на contextLoads добавлен

--- src/test/java/com/kata/cinema_base/CinemaBaseApplicationTests.java
+++ src/test/java/com/kata/cinema_base/CinemaBaseApplicationTests.java
@@ -1,13 +1,21 @@
 package com.kata.cinema_base;
 
+import com.kata.cinema.base.webapp.CinemaBaseApplication;
+import com.kata.cinema.base.webapp.controllers.moderator.ModeratorMovieRestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = CinemaBaseApplication.class)
 class CinemaBaseApplicationTests {
+    @Autowired
+    private ModeratorMovieRestController moderatorMovieRestController;
 
     @Test
     void contextLoads() {
+        assertThat(moderatorMovieRestController).isNotNull();
     }
 
 }
