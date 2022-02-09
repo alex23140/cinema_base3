d184ec74002c241aa41f5d9c443a2ca6a3d6d3b7
Artem Shinkarenko <tomikus1@gmail.com>
2022-02-09 14:57:09 +0300

    post тест для ModeratorMovieRestController добавлен

--- src/test/java/com/kata/cinema/base/webapp/controllers/ModeratorMovieRestControllerTest.java
+++ src/test/java/com/kata/cinema/base/webapp/controllers/ModeratorMovieRestControllerTest.java
@@ -1,7 +1,18 @@
 package com.kata.cinema.base.webapp.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
+import com.jayway.jsonpath.JsonPath;
+import com.kata.cinema.base.models.dto.MovieDto;
+import com.kata.cinema.base.models.enums.MPAA;
+import com.kata.cinema.base.models.enums.RARS;
+import com.kata.cinema.base.service.abstracts.dto.MovieDtoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +25,11 @@ import org.springframework.test.context.support.DependencyInjectionTestExecution
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,14 +45,17 @@ public class ModeratorMovieRestControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private MovieDtoService movieDtoService;
+
     @Test
-    @DatabaseSetup("/dataset/movie.xml")
+    @DatabaseSetup(value = "/dataset/movie.xml", type = DatabaseOperation.CLEAN_INSERT)
+    @DatabaseTearDown(value = "/dataset/movie.xml", type = DatabaseOperation.DELETE_ALL)
     public void shouldReturnMovie() throws Exception {
-        this.mockMvc.perform(get("/api/moderator/movie/2"))
-                .andDo(print())
+        this.mockMvc.perform(get("/api/moderator/movie/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("movie2"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.country").value("rus2"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.dateRelease").value("10.10.2020"))
@@ -47,4 +64,41 @@ public class ModeratorMovieRestControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.previewIsExist").value("true"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.rars").value("TWELVE_PLUS"));
     }
+
+    @Test
+    @DatabaseTearDown(value = "/dataset/movie.xml", type = DatabaseOperation.DELETE_ALL)
+    public void shouldCreateMovie() throws Exception {
+        //создаем сущность (без id)
+        MovieDto movieDto = new MovieDto();
+        movieDto.setName("movie1");
+        movieDto.setCountry("rus1");
+        movieDto.setDateRelease(LocalDate.of(2020, 10, 10));
+        movieDto.setDescription("test1");
+        movieDto.setRars(RARS.EIGHTEEN_PLUS);
+        movieDto.setMpaa(MPAA.GENERAL_AUDIENCES);
+        movieDto.setPreviewIsExist(true);
+        String movieAsJson = getMovieAsJson(movieDto);
+
+        //постим сущность и получаем ее id
+        int id = JsonPath.read(this.mockMvc.perform(post("/api/moderator/movie")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(movieAsJson))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse().getContentAsString(), "$.id");
+
+        //сравниваем отправленную и полученную сущности
+        movieDto.setId((long) id);
+        MovieDto movieFromDb = movieDtoService.getById((long) id);
+        assertThat(movieDto)
+                .usingRecursiveComparison()
+                .isEqualTo(movieFromDb);
+    }
+
+    private String getMovieAsJson(MovieDto movieDto) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsString(movieDto);
+    }
 }
+
