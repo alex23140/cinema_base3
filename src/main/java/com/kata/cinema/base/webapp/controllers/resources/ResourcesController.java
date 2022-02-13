cd7fef154cd531fa026f18496ac040df5908464c
Gorshkov Sergey <Gsn82@mail.ru>
2022-02-13 13:42:21 +0500

    Реализовать ResourcesController

--- src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
@@ -3,23 +3,18 @@ package com.kata.cinema.base.webapp.controllers.resources;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
 
 @Controller
 @Validated
@@ -30,18 +25,23 @@ public class ResourcesController {
     private static String dirpath;
 
 
-    @GetMapping("/uploads/**")
-    public ResponseEntity<Resource> downloadJpg2(HttpServletRequest request) throws Exception {
+    @GetMapping("/uploads/**", produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE} )
+    public ResponseEntity<byte[]> downloadJpg2(HttpServletRequest request) throws Exception {
 
-            String str = httpServletRequestToString(request);
+        String strFile = httpServletRequestToString(request);
 
+        File fileName = new File(strFile);
 
-
-
-
+        if (fileName.exists()) {
+            byte[] bytes = Files.readAllBytes(fileName.toPath());
+            return ResponseEntity.ok().body(bytes);
+        } else {
+            // не существует
+            byte[] bytes = new byte[0];
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bytes);
+        }
     }
-
-
+ 
     String httpServletRequestToString(HttpServletRequest request) throws Exception {
 
         ServletInputStream mServletInputStream = request.getInputStream();
@@ -54,35 +54,8 @@ public class ResourcesController {
                 stringBuilder.append(Character.toString((char) httpInData[i]));
             }
         }
-
         return stringBuilder.toString();
     }
-/*
-    @RequestMapping(value = "/uploads/{jpgName}", method = RequestMethod.GET)
-      public ResponseEntity<Resource> downloadJpg(
-            @PathVariable String jpgName) throws IOException {
 
-        //  Resource downloadResource = new InputStreamResource(soimeinputStream)
-        //  Resource downloadResource = new ByteArrayResource(someByteArray)
-        //  Resource downloadResource = new FileSystemResource(someFile)
-        final ClassPathResource downloadResource = new ClassPathResource(jpgName);
-
-        if (!downloadResource.exists()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-
-        HttpHeaders headers = new HttpHeaders();
-
-        // 1. set the correct content type
-        headers.setContentType(MediaType.IMAGE_JPEG);
-
-        // 2. set the correct content length, maybe stored in a db table
-        headers.setContentLength(downloadResource.contentLength());
-
-        // 3. if you want to force downloads, otherwise attachments might be displayed directly in the brwoser
-        headers.setContentDispositionFormData("attachment", jpgName);
-
-        return new ResponseEntity<>(downloadResource, headers, HttpStatus.OK);
-    }/**/
 }
 
