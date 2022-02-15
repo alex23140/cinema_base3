8b8a6e2d41c6a252c13877851b10ed30041667ee
DmitrBerd <91223020+DmitrBerd@users.noreply.github.com>
2022-02-16 00:14:54 +0300

    Merge branch 'dev' into HEAD
    # Conflicts:
    #	src/main/java/com/kata/cinema/base/dao/abstracts/dto/MovieDtoDao.java
    #	src/main/java/com/kata/cinema/base/dao/impl/dto/MovieDtoDaoImpl.java
    #	src/main/java/com/kata/cinema/base/service/abstracts/dto/MovieDtoService.java
    #	src/test/java/com/kata/cinema_base/CinemaBaseApplicationTests.java

--- src/main/java/com/kata/cinema/base/webapp/controllers/GlobalRestControllerExceptionHandler.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/GlobalRestControllerExceptionHandler.java
@@ -1,21 +1,28 @@
 package com.kata.cinema.base.webapp.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 @ControllerAdvice
 public class GlobalRestControllerExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<String> globalException(Exception exception) {
-        return ResponseEntity.status(400).body(exception.getMessage());
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        exception.printStackTrace(pw);
+        return ResponseEntity.status(500).body(sw.toString());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> constraintViolationException(ConstraintViolationException exception) {
-        return ResponseEntity.status(400).body(exception.getMessage());
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        exception.printStackTrace(pw);
+        return ResponseEntity.status(400).body(sw.toString());
     }
 }
