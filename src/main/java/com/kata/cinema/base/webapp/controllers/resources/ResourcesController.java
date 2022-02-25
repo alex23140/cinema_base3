2ab8d722edac2ef22766927db6952c60ff20d781
Gorshkov Sergey <Gsn82@mail.ru>
2022-02-26 00:06:16 +0500

    Реализовать ResourcesController

--- src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
@@ -2,18 +2,25 @@ package com.kata.cinema.base.webapp.controllers.resources;
 
 
 import lombok.AllArgsConstructor;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 @Controller
 @Validated
@@ -44,6 +51,34 @@ public class ResourcesController {
 
 
     }
+
+    @GetMapping(
+            value = "/get-image-with-media-type",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public @ResponseBody
+    byte[] getImageWithMediaType() throws IOException {
+        InputStream in = getClass()
+                .getResourceAsStream("/com/baeldung/produceimage/image.jpg");
+        return IOUtils.toByteArray(in);
+    }
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @RequestMapping(value = "/image/{imageid}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
+    public @ResponseBody
+    byte[] getImageWithMediaType(@PathVariable int imageid) throws IOException {
+
+        Resource resource = resourceLoader.getResource("classpath:color.jpg");
+        File file = resource.getFile();
+        byte[] fileContent = Files.readAllBytes(file.toPath());
+
+        InputStream in = new ByteArrayInputStream(fileContent);
+        return IOUtils.toByteArray(in);
+    }
+
+
 
 }
 
