cf0bfd8ff6aae7039f673e1c3658feaf8e0b4154
Gorshkov Sergey <Gsn82@mail.ru>
2022-02-28 10:42:39 +0500

    Реализовать ResourcesController

--- src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
@@ -7,19 +7,19 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 
 @Controller
@@ -28,57 +28,30 @@ import java.nio.file.Files;
 @RequestMapping("/uploads")
 public class ResourcesController {
 
-    @GetMapping(produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE} )
-    public byte[] downloadJpg2(String ImageName) {
-
-        // open image
-        File imgPath = new File("/uploads/"+ImageName);
-        BufferedImage bufferedImage = null;
-        try {
-            bufferedImage = ImageIO.read(imgPath);
-            // get DataBufferBytes from Raster
-            WritableRaster raster = bufferedImage .getRaster();
-            DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
-
-            return data.getData();
-        } catch (IOException e) {
-            e.printStackTrace();
-
-
-
-            return null;
-        }
-
-
-    }
-
     @GetMapping(
-            value = "/get-image-with-media-type",
-            produces = MediaType.IMAGE_JPEG_VALUE
+            produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE}
     )
     public @ResponseBody
-    byte[] getImageWithMediaType() throws IOException {
-        InputStream in = getClass()
-                .getResourceAsStream("/com/baeldung/produceimage/image.jpg");
-        return IOUtils.toByteArray(in);
+    ResponseEntity<byte[]> getImageWithMediaType(HttpServletRequest request) {
+
+        String imageName = request.getParameter("imageName");
+        String path = request.getParameter("path");
+
+        HttpHeaders headers = new HttpHeaders();
+        byte[] media = new byte[0];
+
+        try {
+            FileInputStream fin = new FileInputStream(new java.io.File("").getAbsolutePath() + path + "/" + imageName);
+            media = IOUtils.toByteArray(fin);
+            return new ResponseEntity<>(media, headers, HttpStatus.OK);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(media, headers, HttpStatus.BAD_REQUEST);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(media, headers, HttpStatus.BAD_REQUEST);
+        }
+
     }
-
-    @Autowired
-    private ResourceLoader resourceLoader;
-
-    @RequestMapping(value = "/image/{imageid}", method = RequestMethod.GET, produces = MediaType.IMAGE_PNG_VALUE)
-    public @ResponseBody
-    byte[] getImageWithMediaType(@PathVariable int imageid) throws IOException {
-
-        Resource resource = resourceLoader.getResource("classpath:color.jpg");
-        File file = resource.getFile();
-        byte[] fileContent = Files.readAllBytes(file.toPath());
-
-        InputStream in = new ByteArrayInputStream(fileContent);
-        return IOUtils.toByteArray(in);
-    }
-
-
-
 }
 
