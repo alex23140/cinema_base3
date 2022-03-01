734d9be0e0b302624a4828df954122f55d56c1e8
Gorshkov Sergey <Gsn82@mail.ru>
2022-03-01 21:50:30 +0500

    Resolve "Реализовать ResourcesController"

--- src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/resources/ResourcesController.java
@@ -28,7 +28,7 @@ import java.nio.file.Files;
 @RequestMapping("/uploads")
 public class ResourcesController {
 
-    @GetMapping(produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+    @GetMapping(   produces = {MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE}  )
     public ResponseEntity<byte[]> getImageWithMediaType(HttpServletRequest request) {
 
         String imageName = request.getParameter("imageName");
