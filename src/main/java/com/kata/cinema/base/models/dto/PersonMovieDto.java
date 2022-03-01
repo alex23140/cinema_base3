44af7333ddb685d32ea050fbb173dfc8d4d7f2db
Artem Shinkarenko <tomikus1@gmail.com>
2022-03-01 20:25:59 +0300

    MoviePersonInformation изменения в movie_person_to_profession таблице

--- src/main/java/com/kata/cinema/base/models/dto/PersonMovieDto.java
+++ src/main/java/com/kata/cinema/base/models/dto/PersonMovieDto.java
@@ -12,6 +12,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class PersonMovieDto {
     private Long id;
+    //TODO добавить originalFullName в таблицу
     private String fullName;
     private String originalFullName;
     private TypeCharacter type;
--- src/main/java/com/kata/cinema/base/models/entity/Person.java
+++ src/main/java/com/kata/cinema/base/models/entity/Person.java
@@ -29,6 +29,12 @@ public class Person {
     @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "original_first_name")
+    private String originalFirstName;
+
+    @Column(name = "original_lasst_name")
+    private String originalLastName;
+
     @Column(name = "growth")
     private Double growth;
 
